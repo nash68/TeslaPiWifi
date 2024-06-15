575489139cde0eead7af004ad9d3ded16b483080
nash68 <nash68@gmail.com>
2024-06-15 16:41:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download latest from github: 
-# wget https://raw.githubusercontent.com/omiq/piusb/main/setup.sh -O setup.sh
+# wget https://raw.githubusercontent.com/nash68/TeslaPiWifi/main/setup.sh -O setup.sh
 # chmod +x setup.sh
 # sudo ./setup.sh
 
